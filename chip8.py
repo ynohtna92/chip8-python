00d20632ed393559be9085d2d7f2e2c0d98beb55
Alexander Ryzhkov <sash13@bigmir.net>
2012-07-01 23:22:21 +0300

    all opcodes added

--- chip8.py
+++ chip8.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+from random import randint
 
 chip8_fontset =(
     0xF0, 0x90, 0x90, 0x90, 0xF0,
@@ -36,8 +37,8 @@ class Chip8:
 		'''remove this'''
 		self.debug_len = 0
 
-		delay_timer = 0
-		sound_timer = 0
+		self.delay_timer = 0
+		self.sound_timer = 0
 		
 		self.drawFlag = True
 		for i in range(len(chip8_fontset)):
@@ -100,6 +101,12 @@ class Chip8:
 				self.pc +=2
 			except KeyError:
 				print 'opcode error'
+			if self.delay_timer > 0:
+				self.delay_timer-=1
+			if self.sound_timer > 0:
+				if self.sound_timer is 1:
+					print 'beep!'
+				self.sound_timer-=1
 
 	def debugRender(self):
 		'''for y in range(32):
@@ -129,18 +136,38 @@ class Chip8:
 			print 'opcode error 0x0NNN'
         def x1NNN(self):
                 print 'x1NNN'
+		self.pc = self.opcode & 0x0FFF;
         def x2NNN(self):
                 print 'x2NNN'
+		self.stack[self.sp] = self.pc
+		self.pc+=1
+		self.pc = self.opcode & 0x0FFF
         def x3NNN(self):
                 print 'x3NNN'
+		if self.V[(self.opcode & 0x0F00) >> 8] is (self.opcode & 0x00FF):
+			self.pc +=4
+		else:
+			self.pc +=2
         def x4NNN(self):
                 print 'x4NNN'
+                if self.V[(self.opcode & 0x0F00) >> 8] is not (self.opcode & 0x00FF):
+                        self.pc +=4
+                else:
+                        self.pc +=2
         def x5NNN(self):
                 print 'x5NNN'
+                if self.V[(self.opcode & 0x0F00) >> 8] is self.V[(self.opcode & 0x00F0) >> 4]:
+                        self.pc +=4
+                else:
+                        self.pc +=2
         def x6NNN(self):
                 print 'x6NNN'
+		self.V[(self.opcode & 0x0F00) >> 8] = self.opcode & 0x00FF
+		self.pc += 2
         def x7NNN(self):
                 print 'x7NNN'
+                self.V[(self.opcode & 0x0F00) >> 8] += self.opcode & 0x00FF
+                self.pc += 2
         def x8NNN(self):
                 try:
                         self.case_0x8000[self.opcode & 0x000F]()
@@ -148,14 +175,40 @@ class Chip8:
                         print 'opcode error 0x8NNN'
         def x9NNN(self):
                 print 'x9NNN'
+		if self.V[(self.opcode & 0x0F00) >> 8] is not self.V[(self.opcode & 0x00F0) >> 4]:
+                        self.pc +=4
+                else:
+                        self.pc +=2
         def xANNN(self):
                 print 'xANNN'
+		self.I = self.opcode & 0x0FFF
+		self.pc +=2
         def xBNNN(self):
                 print 'xBNNN'
+		self.pc = (self.opcode & 0x0FFF)+self.V[0]
         def xCNNN(self):
                 print 'xCNNN'
+		self.V[(self.opcode & 0x0F00) >> 8] = (randint(1,32767) % 0xFF) & (self.opcode & 0x00FF)
+		self.pc +=2
         def xDNNN(self):
                 print 'xDNNN'
+		x=self.V[(self.opcode & 0x0F00) >> 8]
+		y=self.V[(self.opcode & 0x00F0) >> 4]
+		height = self.opcode & 0x000F
+		pixel = 0
+
+		self.V[0xF] = 0
+		
+		for yline in range(height):
+			pixel = self.memory[self.I+yline]
+			for xline in range(8):
+				if (pixel & ( 0x80 >> xline)) is not 0:
+					print (x + xline + ((y+yline)*64)), 'nyan', x , xline ,y, yline
+					if self.gfx[(x + xline + ((y+yline)*64))]:
+						V[0xF] = 1
+					self.gfx[(x + xline + ((y+yline)*64))] ^= 1
+		self.drawFlag = True;
+		self.pc +=2;
         def xENNN(self):
                 try:
                         self.case_0xE000[self.opcode & 0x00FF]()
@@ -170,51 +223,133 @@ class Chip8:
 	'''0NNN case'''
         def x00E0(self):
                 print 'x00E0'
+		self.gfx = [0x0 for x in range(64*32)]
+		self.drawFlag=True
+		self.pc+=2
         def x000E(self):
                 print 'x000E'
+		self.sp-=1
+		self.pc=self.stack[self.sp]
+		self.pc+=2
 
 	'''x8NNN case'''
         def x8XY0(self):
                 print 'x8XY0'
+		self.V[(self.opcode & 0x0F00) >> 8] = self.V[(self.opcode & 0x00F0) >> 4]
+		self.pc +=2
         def x8XY1(self):
                 print 'x8XY1'
+                self.V[(self.opcode & 0x0F00) >> 8] |= self.V[(self.opcode & 0x00F0) >> 4]
+                self.pc +=2
         def x8XY2(self):
                 print 'x8XY2'
+                self.V[(self.opcode & 0x0F00) >> 8] &= self.V[(self.opcode & 0x00F0) >> 4]
+                self.pc +=2
         def x8XY3(self):
                 print 'x8XY3'
+                self.V[(self.opcode & 0x0F00) >> 8] ^= self.V[(self.opcode & 0x00F0) >> 4]
+                self.pc +=2
         def x8XY4(self):
                 print 'x8XY4'
+		if self.V[(self.opcode & 0x00F0) >> 4] > (0xFF - self.V[(self.opcode & 0x0F00) >> 8]):
+			self.V[0xF]=1
+		else:
+			self.V[0xF]=0
+		self.V[(self.opcode & 0x0F00) >> 8] += self.V[(self.opcode & 0x00F0) >> 4]
+		self.pc += 2
         def x8XY5(self):
                 print 'x8XY5'
+                if self.V[(self.opcode & 0x00F0) >> 4] > self.V[(self.opcode & 0x0F00) >> 8]:
+                        self.V[0xF]=0
+                else:
+                        self.V[0xF]=1
+                self.V[(self.opcode & 0x0F00) >> 8] -= self.V[(self.opcode & 0x00F0) >> 4]
+                self.pc += 2
+
         def x8XY6(self):
                 print 'x8XY6'
+		self.V[0xF]=self.V[(self.opcode & 0x0F00) >> 8] & 0x1
+		self.V[(self.opcode & 0x0F00) >> 8] >>=1
+		self.pc +=2
         def x8XY7(self):
                 print 'x8XY7'
+		if self.V[(self.opcode & 0x0F00) >> 8] > self.V[(self.opcode & 0x00F0) >> 4]:
+			self.V[0xF] = 0
+		else:
+			self.V[0xF] = 1
+		self.V[(self.opcode & 0x0F00) >> 8] = self.V[(self.opcode & 0x00F0) >> 4] - self.V[(self.opcode & 0x0F00) >> 8]
+		self.pc+=2
         def x8XYE(self):
                 print 'x8XYE'
+		self.V[0xF] = self.V[(self.opcode & 0x0F00) >> 8] >> 7
+		self.V[(self.opcode & 0x0F00) >> 8] <<=1
+		pc+=2
 
 	'''xENNN case'''
         def xEX9E(self):
                 print 'xEX9E'
+		if self.key[self.V[(self.opcode & 0x0F00) >> 8]] is not 0:
+			self.pc+=4
+		else:
+			self.pc+=2
         def xEXA1(self):
                 print 'xEXA1'
+                if self.key[self.V[(self.opcode & 0x0F00) >> 8]] is 0:
+                        self.pc+=4
+                else:
+                        self.pc+=2
 
 	'''xFNNN case'''
         def xFX07(self):
                 print 'xFX07'
+		self.V[(self.opcode & 0x0F00) >> 8] = self.delay_timer
+		self.pc+=2
         def xFX0A(self):
                 print 'xFX0A'
+		keyPress = False
+
+		for i in range(16):
+			if self.key[i] is not 0:
+				self.V[(self.opcode & 0x0F00) >> 8] = i
+				keyPress = True
+		if not keyPress:
+			return
+		self.pc+=2
         def xFX15(self):
                 print 'xFX15'
+		self.delay_timer = self.V[(self.opcode & 0x0F00) >> 8]
+		self.pc+=2
         def xFX18(self):
                 print 'xFX18'
+                self.sound_timer = self.V[(self.opcode & 0x0F00) >> 8]
+                self.pc+=2
         def xFX1E(self):
                 print 'xFX1E'
+		if (self.I+self.V[(self.opcode & 0x0F00) >> 8]) > 0xFFF:
+			self.V[0xF]=1
+		else:
+			self.V[0xF]=0
+		self.I += self.V[(self.opcode & 0x0F00) >> 8]
+		self.pc+=2
         def xFX29(self):
                 print 'xFX29'
+		self.I = self.V[(self.opcode & 0x0F00) >> 8] *0x5
+		self.pc+=2
         def xFX33(self):
                 print 'xFX33'
-        def xFX55(self):
-                print 'xFX55'
+		self.memory[I] = self.V[(self.opcode & 0x0F00) >> 8] / 100
+		self.memory[I+1] = (self.V[(self.opcode & 0x0F00) >> 8] / 10) % 10
+		self.memory[I+2] = (self.V[(self.opcode & 0x0F00) >> 8] % 100) % 10
+		self.pc+=2
         def xFX65(self):
                 print 'xFX65'
+		for i in range(((self.opcode & 0x0F00) >> 8)+1):
+			self.V[i] = self.memory[self.I+i]
+		self.I += ((self.opcode & 0x0F00) >> 8)+1
+		self.pc+=2	
+        def xFX55(self):
+                print 'xFX55'
+                for i in range(((self.opcode & 0x0F00) >> 8)+1):
+                        self.memory[self.I+i] = self.V[i]
+                self.I += ((self.opcode & 0x0F00) >> 8)+1 
+                self.pc+=2
